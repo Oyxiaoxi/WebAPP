d00803c14f9b2111a1a86723a7c26b446bf13056
欧阳晓锡 <Oyxiaoxi@hotmail.com>
2016-12-03 14:16:18 +0800

    React-Native WebAPP 运行测试

--- WebAPP/index.ios.js
+++ WebAPP/index.ios.js
@@ -3,18 +3,22 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
+//引入 React
 import React, { Component } from 'react';
+//引入 React Native 组件和 API
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  ScrollView
 } from 'react-native';
-
+// 创建一个组件，class extends
 export default class WebAPP extends Component {
+  // 渲染视图
   render() {
     return (
+      // jsx 语法
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
@@ -26,11 +30,12 @@ export default class WebAPP extends Component {
           Press Cmd+R to reload,{'\n'}
           Cmd+D or shake for dev menu
         </Text>
+        <Text style={{color:'red',fontSize:50}}>Hello World</Text>
       </View>
     );
   }
 }
-
+// StyleSheet 样式类
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -49,5 +54,6 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
-
+// AppRegistry API
+// 注册应用入口
 AppRegistry.registerComponent('WebAPP', () => WebAPP);
