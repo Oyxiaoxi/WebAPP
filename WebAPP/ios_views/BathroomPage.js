d4b6ba11b34d2b3e6ee878c35c8c5f62d0859996
Oyxiaoxi <Oyxiaoxi@hotmail.com>
2016-12-17 17:31:08 +0800

    Merge  Branch

--- WebAPP/ios_views/BathroomPage.js
+++ WebAPP/ios_views/BathroomPage.js
@@ -9,22 +9,15 @@ import {
   WebView
 } from 'react-native';
 
-<<<<<<< HEAD
 import TwebView from './TWebView';
-=======
-import TwebView from './TwebView';
->>>>>>> b084fb449f84b081717a28e8b9d7cd3620309e96
+
 
 class BathroomPage extends Component{
     render(){
-        // 必须返回元素 <TwebView url="https://xxx.github.com/facebook/react-native"/>
+        // 必须返回元素  <TwebView url="{require('../html/nearby.html')} "/>
         return(
             <View style={styles.container}>
-<<<<<<< HEAD
-                <TwebView url="{require('../html/nearby.html')} "/>
-=======
                 <TwebView url="https://github.com/facebook/react-native"/>
->>>>>>> b084fb449f84b081717a28e8b9d7cd3620309e96
             </View>
         );
     }
